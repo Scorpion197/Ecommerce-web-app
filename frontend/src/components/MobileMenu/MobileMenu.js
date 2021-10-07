993ddb70732646c98c36121f1e94426c20a0e261
Scorpion197 <doudou.gaw@gmail.com>
2021-10-07 17:52:48 +0200

    Fixed mobile menu responsiveness

--- frontend/src/components/MobileMenu/MobileMenu.js
+++ frontend/src/components/MobileMenu/MobileMenu.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
 // styles 
 import {Wrapper, Menu} from './MobileMenu.styles';
+//hooks 
+import { useState } from 'react';
 
 const MobileMenu = () => {
 
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => {
+
+        setClick(!click);
+    }
 
     return (
 
@@ -13,7 +22,11 @@ const MobileMenu = () => {
             <Wrapper>
 
                 <h1 className="time-zone"> TimeZone</h1>
-                <MenuIcon className="icon" style={{color: "#000"}}/>
+                <div className="icon-item" onClick={handleClick}>
+                    {
+                        click ? <CloseIcon className="icon" style={{color: "#000"}}/> : <MenuIcon className="icon" style={{color: "#000"}}/>
+                    }
+                </div>
             </Wrapper>
         </>
     )
