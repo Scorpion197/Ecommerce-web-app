da30515d90ebf068f70762c3d4fa79cf5d0fbe34
Scorpion197 <doudou.gaw@gmail.com>
2021-10-08 18:37:27 +0200

    Finished rest API

---
+++ backend/backApp/API/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+
+from .views import ProductListView, ProductDetailView
+
+urlpatterns = [
+
+    path('', ProductListView.as_view(), name='all_products'),
+    path('<pk>', ProductDetailView.as_view(), name='product_detail'),
+]
