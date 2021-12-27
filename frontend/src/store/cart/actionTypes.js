991a2d8ba68345c52d54c89390fd2dc55eb0d187
Scorpion197 <doudou.gaw@gmail.com>
2021-12-27 21:51:04 +0100

    Added more actions into redux store

--- frontend/src/store/cart/actionTypes.js
+++ frontend/src/store/cart/actionTypes.js
@@ -1,2 +1,5 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const POST_ADD_TO_CART = "POST_ADD_TO_CART";
+export const ADDED_TO_CART = "ADDED_TO_CART";
+export const FAILED_ADD_TO_CART = "FAILED_ADD_TO_CART";
--- frontend/src/store/cart/cart.js
+++ frontend/src/store/cart/cart.js
@@ -2,11 +2,22 @@ import * as actionTypes from './actionTypes';
 
 export const addToCart = (productName, productPrice) =>{
 
+    
     return {
 
         type: actionTypes.ADD_TO_CART, 
         productName: productName, 
         productPrice: productPrice,
+    }
+
+}
+
+export const postAddToCart = (productName, productPrice) => {
+
+    return dispatch => {
+
+        dispatch(addToCart(productName, productPrice));
+        
     }
 
 }
